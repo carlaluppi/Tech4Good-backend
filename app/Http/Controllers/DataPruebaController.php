a832bb4e63dfbf75e3a8c4eb96a0ee3384bf1080
carlilu <carluppi99@gmail.com>
2023-06-20 15:13:08 +0200

    created json data

---
+++ app/Http/Controllers/DataPruebaController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DataPruebaController extends Controller
+{
+    public function index()
+    {
+        $path = public_path('dataPrueba.json');
+        $data = file_get_contents($path);
+        
+        return response()->json(json_decode($data, true));
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\SequiaController;
+use App\Http\Controllers\DataPruebaController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +19,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::resource('/data', DataPruebaController::class);
+
+
+Route::resource('sequia', SequiaController::class);

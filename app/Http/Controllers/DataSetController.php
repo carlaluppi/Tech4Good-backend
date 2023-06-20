9d837495c238aba2057b6a3be191396b37c7e16e
carlilu <carluppi99@gmail.com>
2023-06-20 16:57:59 +0200

    data from 2000

--- app/Http/Controllers/DataSetController.php
+++ app/Http/Controllers/DataSetController.php
@@ -4,12 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class DataPruebaController extends Controller
+class DataSetController extends Controller
 {
     public function index()
     {
         $path = public_path('dataset.json');
         $data = file_get_contents($path);
+        // $path = $path.
         
         return response()->json(json_decode($data, true));
     }
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\SequiaController;
-use App\Http\Controllers\DataPruebaController;
+use App\Http\Controllers\DataSetController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,7 +20,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::resource('/data', DataPruebaController::class);
+Route::resource('/data', DataSetController::class);
 
 
-Route::resource('sequia', SequiaController::class);
